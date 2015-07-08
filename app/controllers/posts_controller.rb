65896d6611446cf80b30fc9711b5a7f2cff07146
Michael Harvey <michaelharvey17@gmail.com>
2015-07-08 11:42:21 -0400

    Added content to posts_controller.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,3 +1,15 @@
 class PostsController < ApplicationController
+  def new
+    @post=Post.new
+
+  end
+
+  def create
+    @post=Post.new(params[:post])
+
+  end
+
+  def delete
+    Post.find(params[:id]).destroy
 
 end 
