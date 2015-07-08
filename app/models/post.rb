020506099eda2e194f1bc4f07bf24b5660e38d44
Berlin Patience <starsberlin@gmail.com>
2015-07-08 11:25:03 -0400

    new

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,7 @@
 class Post < ActiveRecord::Base
+  belongs_to :user
+  has_many :comments, class_name: "Post",
+                      foreign_key: "post_id"
+  validates :body, presence: true
+  belongs_to :main_post, class_name: "Post"
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,7 @@
 class User < ActiveRecord::Base
+  has_many :posts
+  validates :password, confirmation: true
+  validates_uniqueness_of :email, :username
+  validates_presence_of :username, :email, :password, :lname, :fname, :password_confirmation
+
 end
