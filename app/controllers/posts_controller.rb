863aa8662efebd67270f1cb1786c52c40cbc31fa
Michael Harvey <michaelharvey17@gmail.com>
2015-07-08 14:34:54 -0400

    Created post page.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -9,7 +9,17 @@ class PostsController < ApplicationController
 
   end
 
+  def edit
+    @post=Post.find(params[:id])
+    Post.find(params[:id]).update(content: params[:content])
+  end
+
   def delete
     Post.find(params[:id]).destroy
+  end
+
+  def show
+    @post=Post.find(params[:id])
+  end
 
 end 
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  resources :users
+  resources :posts
+
+  root 'home#index'
+  put '/edit', to: "post#edit"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
